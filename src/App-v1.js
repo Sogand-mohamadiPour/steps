0b632aaa4cc7759946ee3f4dc6736e7c78849bb4
Sogand <sogandmohamadipour7@gmail.com>
2025-09-07 17:27:54 +0330

    a new version testing children prop

--- src/App-v1.js
+++ src/App-v1.js
@@ -29,51 +29,22 @@ export default function App() {
             <div className={step >= 3 ? 'active' : ""}>3</div>
           </div>
 
-          <StepMessage step={step}>
-            {messages[step - 1]}
-          </StepMessage>
-
+          <p className="message">
+            step{step}:{messages[step - 1]}
+          </p>
           <div className="buttons">
-            <Button
-              bgColor='#7950f2'
-              textColor='#fff'
+            <button style={{ backgroundColor: '#7950f2', color: '#fff' }}
               onClick={handlePrevious}
-            > <span>👈</span> Previous
-            </Button>
-            <Button
-              bgColor='#7950f2'
-              textColor='#fff'
+            >Previous
+            </button>
+            <button style={{ backgroundColor: '#7950f2', color: '#fff' }}
               onClick={handleNext}
-            >
-              Next <span>👉</span>
-            </Button>
+            >Next
+            </button>
           </div>
         </div>
       )}
     </>
 
-  )
-}
-
-function StepMessage({step, children}) {
-  return (
-  <>
-    <div className="message">
-      <h3>step {step}</h3>
-      {children}
-    </div>
-  </>
-  )
-}
-
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <>
-      <button style={{ backgroundColor: bgColor, color: textColor }}
-        onClick={onClick}>
-        {children}
-      </button>
-    </>
   )
 }
